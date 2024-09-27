5ded9aa5701cec782daa01dd3630a955ea14f88d
duanegero <duanegero11@gmail.com>
2024-09-27 11:12:38 -0300

    adding background color, font color and list to display each selection

--- main.js
+++ main.js
@@ -15,8 +15,53 @@ const updateTime = () => {
 
     dateTimeDisplay.textContent = `${selectedTime} current time is ${currentTime}`
 
+    const timezoneList = document.getElementById('time-zone-list');
+    const newItem = document.createElement('li');
+    newItem.innerText = dateTimeDisplay.textContent;
+    timezoneList.appendChild(newItem);
 }
 
-timeZoneDropdown.addEventListener('change', updateTime);
+const backgroundColor = () => {
+    const selectedOption = timeZoneDropdown.options[timeZoneDropdown.selectedIndex];
+    const optionId = selectedOption.id;
+    
+    if(optionId == 'africa'){
+        document.body.style.backgroundColor = 'red';
+        document.getElementById('clock').style.color = 'white';
+        document.getElementById('timeZone-lable').style.color = 'white';
+        document.getElementById('time-zone-list').style.color = 'white';
+    }else if(optionId == 'america'){
+        document.body.style.backgroundColor = 'blue';
+        document.getElementById('clock').style.color = 'white';
+        document.getElementById('timeZone-lable').style.color = 'white';
+        document.getElementById('time-zone-list').style.color = 'white';
+    }else if(optionId == 'asia'){
+        document.body.style.backgroundColor = 'yellow';
+        document.getElementById('clock').style.color = 'black';
+        document.getElementById('timeZone-lable').style.color = 'black';
+        document.getElementById('time-zone-list').style.color = 'black';
+    }else if(optionId == 'australia'){
+        document.body.style.backgroundColor = 'purple';
+        document.getElementById('clock').style.color = 'black';
+        document.getElementById('timeZone-lable').style.color = 'black';
+        document.getElementById('time-zone-list').style.color = 'black';
+    }else if(optionId == 'europe'){
+        document.body.style.backgroundColor = 'green';
+        
+        document.getElementById('clock').style.color = 'black';
+        document.getElementById('timeZone-lable').style.color = 'black';
+        document.getElementById('time-zone-list').style.color = 'black';
+    }else if(optionId == 'pacific'){
+        document.body.style.backgroundColor = 'orange';
+        document.getElementById('clock').style.color = 'black';
+        document.getElementById('timeZone-lable').style.color = 'black';
+        document.getElementById('time-zone-list').style.color = 'black';
+    }
+}
+
+timeZoneDropdown.addEventListener('change', () => {
+    updateTime();
+    backgroundColor();
+});
 
 window.onload = updateTime;
